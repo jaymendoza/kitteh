d83626ce9b22250e8868abc4b6c55654248eb672
Jay Mendoza <jay.mendoza@bigcommerce.com>
2017-04-25 14:35:31 -0500

    add basic working csv functionality

--- Gemfile
+++ Gemfile
@@ -1,4 +1,3 @@
 source 'https://rubygems.org'
 gem 'dotenv-rails', groups: [:development, :test]
 gem 'bigcommerce', '~> 1.0.0'
-gem 'smarter_csv'
--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'bundler'
 require 'dotenv'
+require 'csv'
 
 Bundler.require
 Dotenv.load
@@ -11,45 +12,13 @@ Bigcommerce.configure do |config|
   config.api_key = ENV['API_KEY']
 end
 
-class OrderExporter 
-  PRODUCT_FIELDS = ['name', 'sku', 'base_price']
-  SHIPPING_ADDRESS_FIELDS = ['first_name']
-  COUPON_FIELDS = ['code', 'amount', 'discount']
-
-  def self.handle_subresource(sub)
-    resource = sub[:resource].split('/').last
-    id = sub[:resource].match(/\d+/)[0]
-
-    case resource
-    when 'products'
-      build_resource_string('Product', id, PRODUCT_FIELDS)
-    when 'shippingaddresses'
-      build_resource_string('ShippingAddress', id, SHIPPING_ADDRESS_FIELDS)
-    when 'coupons'
-      build_resource_string('Coupon', id, COUPON_FIELDS)
-    end
-  end
-
-  def self.build_resource_string(resource, id, fields)
-    full_class_path = 'Bigcommerce::Order' + resource
-    klass = Module.const_get(full_class_path)
-    cell_contents = ''
-
-    klass.all(id.to_i).each do |rsc|
-      resource_string = ''
-      fields.each do |field|
-        if !rsc.send(field).empty?
-          resource_string << "#{field}: #{rsc.send(field)}, "
-        end
-      end
-      cell_contents << resource_string.chomp(', ') + ' -- '
-    end
-
-    cell_contents.chomp(' -- ')
+class Kitteh
+  def self.column_headers
+    Bigcommerce::Category.all.first.keys.map {|key| key.to_s }
   end
 end
 
-#CSV.open('testsub1.csv', 'a') {|csv| csv << Bigcommerce::Order.all.first.keys }
+CSV.open('testsub1.csv', 'a') {|csv| csv << Kitteh.column_headers }
 
 Bigcommerce::Category.all.each do |cat|
   puts cat.name
